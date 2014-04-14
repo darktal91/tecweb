9f1f348819ed3703c108609b9fa6a94e4dae2353
gmarcomi <gabriele.marcomin@gmail.com>
2014-04-14 13:39:33 +0200

    aggiornamento dao

--- cgi-bin/Database.pm
+++ cgi-bin/Database.pm
@@ -5,11 +5,34 @@ use warnings;
 use diagnostics;
 use XML::LibXML;
 
+package Database;
+
+
 
 my $parser=XML::LibXML->new();
 my $padiglioni='/data/padiglioni/padiglioni.xml';
+sub new{
+    my $class = shift;
+    my $self = {
+        _firstName => shift,
+        _lastName  => shift,
+        _ssn       => shift,
+    };
+    # Print all the values just for clarification.
+    print "First Name is $self->{_firstName}\n";
+    print "Last Name is $self->{_lastName}\n";
+    print "SSN is $self->{_ssn}\n";
+    bless $self, $class;
+    return $self;
+}
+sub login{}
+sub register{}
+sub insert_comment{}
+sub insert_ticket{}
 
+sub update_password{}
 
+sub delete_user{}
 
 sub getPad4id{
   my $doc=$parser->parse_file($padiglioni);
